e2fb82bea2b71e41b9c7d78b833632c54b3ab7ed
wzp301 <879125906@qq.com>
2021-10-13 10:47:58 +0800

    Fix JPBC_Test project configuration and Add JPBC library file

--- JPBC_Test/src/com/test/JPBCTest.java
+++ JPBC_Test/src/com/test/JPBCTest.java
@@ -5,7 +5,6 @@ import it.unisa.dia.gas.jpbc.Field;
 import it.unisa.dia.gas.jpbc.Pairing;
 import it.unisa.dia.gas.plaf.jpbc.pairing.PairingFactory;
 
-import javax.rmi.CORBA.Util;
 import java.nio.charset.Charset;
 
 
@@ -13,28 +12,29 @@ public class JPBCTest {
 
     public static void main(String[] args) {
 
-//        Pairing bp = PairingFactory.getPairing("a.properties");
-//
-//        Field G1 = bp.getG1();
-//        Field Zr = bp.getZr();
-//
-//        Element g = G1.newRandomElement();
-//        Element a = Zr.newRandomElement();
-//        Element b = Zr.newRandomElement();
-//
-//        Element g_a = g.duplicate().powZn(a);
-//        Element g_b = g.duplicate().powZn(b);
-//        Element egg_ab = bp.pairing(g_a, g_b);
-//
-//        Element egg = bp.pairing(g, g);
-//        Element ab = a.duplicate().mul(b);
-//        Element egg_ab_p = bp.pairing(g_a, g_b);
-//
-//        if (egg_ab.isEqual(egg_ab_p)) {
-//            System.out.println("Yes");
-//        } else {
-//            System.out.println("No");
-//        }
+        Pairing bp = PairingFactory.getPairing("a.properties");
+
+        Field G1 = bp.getG1();
+        Field Zr = bp.getZr();
+
+        Element g = G1.newRandomElement();
+        Element a = Zr.newRandomElement();
+        Element b = Zr.newRandomElement();
+
+        Element g_a = g.duplicate().powZn(a);
+        Element g_b = g.duplicate().powZn(b);
+        Element egg_ab = bp.pairing(g_a, g_b);
+
+        Element egg = bp.pairing(g, g);
+        Element ab = a.duplicate().mul(b);
+        Element egg_ab_p = bp.pairing(g_a, g_b);
+
+        if (egg_ab.isEqual(egg_ab_p)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
         try (InitTimer it = Utils.timer("Sha256 Test")) {
             String plaintext = "hello";
             byte [] byteString = plaintext.getBytes(Charset.forName("UTF-8"));
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # JPBC
-JPBC学习代码示例
+
+JPBC是一个基于双线性对的密码学Java工具库，本项目主要展示一些JPBC的学习代码示例。
+
+# JPBC学习资料
+
+JPBC官网: [The Java Pairing Based Cryptography Library (JPBC)](http://gas.dia.unisa.it/projects/jpbc/docs/element.html#initializing)
+
+JPBC库下载地址：[Download JPBC](http://prdownloads.sourceforge.net/jpbc/?sort_by=date&sort=desc), 库已经下载到仓库的根目录下
+
+JPBC环境搭建视频教程：[JPBC库（基于配对的密码学）入门和避坑指南](https://www.bilibili.com/video/BV1o5411Y77r?spm_id_from=333.999.0.0)
